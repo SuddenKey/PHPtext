9cb37f4d6d7084862df5d2423f56647ba31cb53e
haohao <921275510@qq.com>
2017-07-29 14:28:17 +0800

    修改用户资料

--- db/edituser.php
+++ db/edituser.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>编辑用户</title>
+</head>
+<body>
+
 <?php
 /**
  * Created by PhpStorm.
@@ -5,3 +13,22 @@
  * Date: 2017/7/28
  * Time: 下午4:23
  */
+
+require_once 'function.php';
+if (!empty($_GET['id'])) {
+    $conn = connectDB();
+    $id = intval($_GET['id']);
+    $sql = "select * form users where id = $id";
+
+    $result = $conn->query($sql);
+    if (mysqli_errno($conn)) {
+        die('can not connect db');
+    }
+    $arr = mysqli_fetch_assoc($result);
+    print_r($arr);
+}
+?>
+
+</body>
+</html>
+
